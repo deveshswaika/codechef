97f25afb361d900d8311f8884d1a5acee45217d8
deveshswaika <deveshswaika@gmail.com>
2018-06-22 07:39:08 +0530

    Addressing Review Comments

--- Approximately.java
+++ Approximately.java
@@ -8,24 +8,23 @@ class Approximately {
         Scanner sc = new Scanner(System.in);
         int num_test_cases = sc.nextInt();
         int num_decimal_digits;
-        int temp, truncate, num_int_part, NUMERATOR, DENOMINATOR;
-        truncate = 33102;
+        int temp, int_part, NUMERATOR, DENOMINATOR;
         NUMERATOR = 103993;
         DENOMINATOR = 33102;
-        num_int_part = 3;
+        int_part = 3;
         for (int i = 0; i < num_test_cases; i++) {
             int remainder = NUMERATOR % DENOMINATOR;
             num_decimal_digits = sc.nextInt();
             int digits[] = new int[num_decimal_digits];
             if (num_decimal_digits == 0) {
-                System.out.println(num_int_part);
+                System.out.println(int_part);
             }
             else {
                 int j = 0;
                 while (j < num_decimal_digits) {
                     remainder *= 10;
-                    digits[j] = remainder / truncate;
-                    remainder = remainder % truncate;
+                    digits[j] = remainder / DENOMINATOR;
+                    remainder = remainder % DENOMINATOR;
                     j++;
                 }
                 System.out.print("3.");
