05825ad19b08daa766c57c38a29e4b212410af3b
deveshswaika <deveshswaika@gmail.com>
2018-06-22 06:50:16 +0530

    Updating Variable Names

--- Approximately.java
+++ Approximately.java
@@ -6,29 +6,29 @@ import java.util.*;
 class Approximately {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        int num_test_cases = sc.nextInt();
         int k;
-        int temp, truncate, ans;
+        int temp, truncate, num_int_part;
         truncate = 33102;
-        ans = 3;
-        for (int i = 0; i < t; i++) {
+        num_int_part = 3;
+        for (int i = 0; i < num_test_cases; i++) {
             int remainder = 4687;
             k = sc.nextInt();
-            int a[] = new int[k];
+            int num_decimal_digit[] = new int[k];
             if (k == 0) {
-                System.out.println(ans);
+                System.out.println(num_int_part);
             }
             else {
                 int j = 0;
                 while (j < k) {
                     remainder *= 10;
-                    a[j] = remainder / truncate;
+                    num_decimal_digit[j] = remainder / truncate;
                     remainder = remainder % truncate;
                     j++;
                 }
                 System.out.print("3.");
                 for (int x = 0; x < k; x++) {
-                    System.out.print(a[x]);
+                    System.out.print(num_decimal_digit[x]);
                 }
                 System.out.println();
             }
