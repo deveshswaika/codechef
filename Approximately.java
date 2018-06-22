fe8fffe92c2a469036d78f4d9bc122a1b156ad24
deveshswaika <deveshswaika@gmail.com>
2018-06-22 08:55:21 +0530

    Addressing Review Comments

--- Approximately.java
+++ Approximately.java
@@ -11,27 +11,33 @@ class Approximately {
         int DENOMINATOR = 33102;
         int int_part = NUMERATOR / DENOMINATOR;
         for (int i = 0; i < num_test_cases; i++) {
+            int num_decimal_digits = sc.nextInt();           
+            String truncated_number = calculate_fraction(NUMERATOR,DENOMINATOR,num_decimal_digits);
+            System.out.println(truncated_number);
+        }
+    }
+    
+    public static String calculate_fraction(int NUMERATOR,int DENOMINATOR,int num_decimal_digits) {
+        if(num_decimal_digits==0) {
+            return String.valueOf(NUMERATOR / DENOMINATOR);
+        }
+        else {
             int remainder = NUMERATOR % DENOMINATOR;
-            int num_decimal_digits = sc.nextInt();
             int digits[] = new int[num_decimal_digits];
-            if (num_decimal_digits == 0) {
-                System.out.println(int_part);
+            int j = 0;
+            while (j < num_decimal_digits) {
+                remainder *= 10;
+                digits[j] = remainder / DENOMINATOR;
+                remainder = remainder % DENOMINATOR;
+                j++;
             }
-            else {
-                int j = 0;
-                while (j < num_decimal_digits) {
-                    remainder *= 10;
-                    digits[j] = remainder / DENOMINATOR;
-                    remainder = remainder % DENOMINATOR;
-                    j++;
-                }
-                System.out.print(String.valueOf(int_part) + ".");
-                for (int x = 0; x < num_decimal_digits; x++) {
-                    System.out.print(digits[x]);
-                }
-                System.out.println();
+            String truncated_num =  String.valueOf(NUMERATOR / DENOMINATOR) + ".";
+            for (int x = 0; x < num_decimal_digits; x++) {
+                truncated_num = truncated_num.concat(String.valueOf(digits[x]));
             }
+            return truncated_num;
         }
     }
 }
+
 
